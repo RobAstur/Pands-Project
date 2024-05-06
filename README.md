d4b0f4b55a04102717c717112526f954a9545ebc
Roberto Gomez Garcia <gomezgar.roberto@gmail.com>
2024-05-06 10:21:53 +0100

    uploading libraries

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pands-Project
-Pands-project
+ - objective Pands-project
+
+ Research the data set online and write a summary about it in your README.  
---
+++ iris_project.py
@@ -0,0 +1,9 @@
+# Import libraries
+
+# Data frames
+import pandas as pd
+#Plotting
+import matplotlib.pyplot as plt
+#numerical arrays
+import numpy as np
+
